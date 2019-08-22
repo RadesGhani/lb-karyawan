e04f014296009fefa11bd49438591ae6873c082c
RadesGhani <rades208@gmail.com>
2019-08-22 10:22:16 +0700

    update user.json & cuti.js
    Recreate ACL for user.json :
    user, kehadiran, cuti, inventaris
    
    Remove redundant remote method in cuti.js
    
    Ignore datasources.json

--- common/models/cuti.js
+++ common/models/cuti.js
@@ -1,15 +1,5 @@
 'use strict';
 
 module.exports = function(Cuti) {
-    Cuti.observe('before save', function updateUserId(ctx, next){
-        var userId = ctx.options.accessToken.userId
-
-        if (ctx.instance) {
-            ctx.instance.user_id = userId;
-        } else {
-            ctx.data.user_id = userId;
-        }
-        next();
-    });
-
+    
 };
