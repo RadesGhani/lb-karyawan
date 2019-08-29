9d37c2ef163690d4d2965195b51103bc108b77d6
akhmed2712 <baidarus215@gmail.com>
2019-08-29 14:29:01 +0700

    profile relation user, salary relation, username and password validation

--- common/models/profile.js
+++ common/models/profile.js
@@ -2,4 +2,42 @@
 
 module.exports = function(Profile) {
     Profile.validatesUniquenessOf('code_name');
+    Profile.validatesInclusionOf('sex', {in: ['male', 'female']});
+
+    Profile.observe('before save', function(ctx, next){
+        if (ctx.instance && ctx.instance.user_Id){
+             return Profile.app.models.user.count({id: 
+             ctx.instance.user_Id
+             }).then(
+                 res => {
+                     if (res < 1){
+                         var err = {
+                             statusCode: "400",
+                             message: "Penambahan profile error, username tidak eksis"
+                         };
+                         return Promise.reject(err);
+                     }
+                }
+             )
+         }
+         return next();
+     });
+     Profile.observe('before save', function(ctx, next){
+     if (ctx.instance && ctx.instance.user_Id){
+        return Profile.app.models.profile.count({user_Id: 
+        ctx.instance.user_Id
+        }).then(
+            res => {
+                if (res >= 1){
+                    var err = {
+                        statusCode: "400",
+                        message: "Penambahan profile error, username sudah diisi profile"
+                    };
+                    return Promise.reject(err);
+                }
+           }
+        )
+    }
+    return next();
+});
 };
