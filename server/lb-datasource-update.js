5915d5d8d74672bf3caf4cc1184122694380dfb4
Aditya-090 <akun.gugelku00@gmail.com>
2019-09-05 09:30:14 +0700

    salary

--- server/lb-datasource-update.js
+++ server/lb-datasource-update.js
@@ -1,6 +1,6 @@
 var server = require('./server');
-var ds = server.dataSources.postgresDs;
-var lbTables = ['inventaris'];
+var ds = server.dataSources.mongoDs;
+var lbTables = ['salary'];
 ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
