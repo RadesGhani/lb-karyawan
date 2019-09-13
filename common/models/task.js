71c5c733c9f41b5d446203b85b63ac7c02663703
akhmed2712 <baidarus215@gmail.com>
2019-09-13 15:32:32 +0700

    relasi task-team_project-user

--- common/models/task.js
+++ common/models/task.js
@@ -1,44 +1,50 @@
+/* eslint-disable camelcase */
 'use strict';
 
-//userID task
-
-    
+// userID task
 
 module.exports = function(Task) {
-    Task.observe('before save', function(ctx, next){
-       if (ctx.instance && ctx.instance.team_project){
-            return Task.app.models.team_project.count({id: 
-            ctx.instance.team_project
-            }).then(
-                res => {
-                    if (res < 1){
-                        var err = {
-                            statusCode: "400",
-                            message: "Penambahan tugas error, team_project tidak eksis"
-                        };
-                        return Promise.reject(err);
-                    }
-               }
-            )
-        }
-        return next();
-    });
-    Task.observe('before save', function(ctx, next){
-      if (ctx.instance && ctx.instance.user_id){
-          return Task.app.models.user.count({id: 
-          ctx.instance.user_id
-          }).then(
-              res => {
-                  if (res < 1){
-                      var err = {
-                          statusCode: "400",
-                          message: "Penambahan tugas error, username tidak eksis"
-                    };
-                      return Promise.reject(err);
-                  }
-              }
-          )
+  Task.observe('access', function limitToTenant(ctx, next) {
+    var authorizedRoles = ctx.options.authorizedRoles;
+    var storeId = ctx.options.currentUser.team_name_id;
+
+    if (!authorizedRoles.admin) {
+      ctx.query.where = ctx.query.where || {};
+      ctx.query.where.team_name_id = storeId;
+    }
+    next();
+  });
+
+  Task.observe('before save', function(ctx, next) {
+    if (ctx.instance && ctx.instance.team_name_id) {
+      return Task.app.models.team_project.count({id:
+      ctx.instance.team_name_id,
+      }).then(
+          res => {
+            if (res < 1) {
+              var err = {
+                statusCode: '400',
+                message: 'Penambahan task error, team_project tidak eksis',
+              };
+              return Promise.reject(err);
+            }
+          }
+      );
+    }
+    return next();
+  });
+      // only admin in each team_project who can only create task team_project in group that they own as admin
+  Task.observe('before save', function(ctx, next) {
+    var teamId = ctx.options.currentUser.team_name_id;
+    if (ctx.instance && ctx.instance.team_name_id) {
+      if (ctx.instance.team_name_id != teamId) {
+        var err = {
+          statusCode: '401',
+          message: 'Authorization Required',
+        };
+        return Promise.reject(err);
       }
-      return next();
+    }
+    return next();
   });
 };
--- common/models/team_project.js
+++ common/models/team_project.js
@@ -1,22 +1,6 @@
+/* eslint-disable camelcase */
+
 'use strict';
 
 module.exports = function(Team_project) {
-    Team_project.observe('before save', function(ctx, next){
-        if (ctx.instance && ctx.instance.name_team){
-            return Team_project.app.models.team_project.count({name_team: 
-            ctx.instance.name_team
-            }).then(
-                res => {
-                    if (res >= 1){
-                        var err = {
-                            statusCode: "400",
-                            message: "Penambahan team_project error, team_project sudah ada"
-                        };
-                        return Promise.reject(err);
-                    }
-                }
-            )
-        }
-        return next();
-    });
 };
