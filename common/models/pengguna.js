e4397c0451ca72d172b068de1c25142dde7f0a05
RadesGhani <rades208@gmail.com>
2019-09-19 10:16:05 +0700

    Merge rades2

--- common/models/pengguna.js
+++ common/models/pengguna.js
@@ -20,89 +20,96 @@ module.exports = function(Pengguna) {
   Pengguna.validatesLengthOf('username', {min: 2}); //username length
   Pengguna.validatesLengthOf('username', {max: 50}); //username length
   Pengguna.validatesFormatOf('username', {with: /^[A-Za-z\\s]*$/});//username validation(only character type will be accepted)
-  //logic untuk mengirim email selamat bergabung
-  Pengguna.afterRemote('create', function(context, pengguna, next) {
-    Pengguna.app.models.Email.send({
-      to: pengguna.email,
-      from: senderAddress,
-      subject: 'Selamat bergabung di perusahaan kami.',
-      html: 'Selamat bergabung di perusahaan kami. Username untuk mengakses akun pegawai anda adalah <b>' + pengguna.username + '</b>. Harap selesaikan registrasi akun pegawai anda dengan melakukan reset password melalui link berikut : ' + frontend + ' atau gunakan aplikasi mobile perusahaan.'
-    }, function(err) {
-      if (err){
-        Pengguna.deleteById(pengguna.id);
-        return next(err);
-      }
+  
+  {//logika untuk mengirim email selamat bergabung
+    Pengguna.afterRemote('create', function(context, pengguna, next) {
+      Pengguna.app.models.Email.send({
+        to: pengguna.email,
+        from: senderAddress,
+        subject: 'Selamat bergabung di perusahaan kami.',
+        html: 'Selamat bergabung di perusahaan kami. Username untuk mengakses akun pegawai anda adalah <b>' + pengguna.username + '</b>. Harap selesaikan registrasi akun pegawai anda dengan melakukan reset password melalui link berikut : ' + frontend + ' atau gunakan aplikasi mobile perusahaan.'
+      }, function(err) {
+        if (err){
+          Pengguna.deleteById(pengguna.id);
+          return next(err);
+        }
         return next();
       })
     });
-  //send password reset link when requested
-  Pengguna.on('resetPasswordRequest', function(info) {
-    var url = 'https://' + config.host + ':' + config.port + '/reset-password';
-    /*var html = 'Click <a href="' + url + '?access_token=' +
-        info.accessToken.id + '">here</a> to reset your password';*/
-      var html = 'access token untuk mereset password anda: '+ info.accessToken.id;
+  }
 
-    Pengguna.app.models.Email.send({
-      to: info.email,
-      from: senderAddress,
-      subject: 'Password reset',
-      html: html
-    }, function(err) {
-      if (err) return console.log('> error sending password reset email');
-      console.log('> sending password reset email to:', info.email);
+  {//logika reset password
+    Pengguna.on('resetPasswordRequest', function(info) {
+      var url = 'https://' + config.host + ':' + config.port + '/reset-password';
+      /*var html = 'Click <a href="' + url + '?access_token=' +
+          info.accessToken.id + '">here</a> to reset your password';*/
+        var html = 'access token untuk mereset password anda: '+ info.accessToken.id;
+
+      Pengguna.app.models.Email.send({
+        to: info.email,
+        from: senderAddress,
+        subject: 'Password reset',
+        html: html
+      }, function(err) {
+        if (err) return console.log('> error sending password reset email');
+        console.log('> sending password reset email to:', info.email);
+      });
     });
-  });
+  }
 
-//password validation
+  {//logika validasi password
+    Pengguna.validatePassword = function(json) {
+      var err,
+          passwordProperties = Pengguna.definition.properties.password;
 
-  Pengguna.validatePassword = function(json) {
-    var err,
-        passwordProperties = Pengguna.definition.properties.password;
+      if (json.length > passwordProperties.max) {
+        err = new Error (g.f('Password terlalu panjang: %s (maksimal %d karakter)', json, passwordProperties.max));
+        err.code = 'PASSWORD_TOO_LONG';
+      } else if (json.length < passwordProperties.min) {
+        err = new Error(g.f('Password terlalu singkat: %s (minimal %d karakter)', json, passwordProperties.min));
+        err.code = 'PASSWORD_TOO_SHORT';
+      } else if(!(new RegExp(passwordProperties.pattern, 'g').test(json))) {
+        err =  new Error(g.f('password salah: %s (masukkan huruf kapital, huruf kecil, simbol, dan angka)', json));
+        err.code = 'INVALID_PASSWORD';
+      } else {
+        return true;
+      }
+      err.statusCode = 422;
+      throw err;
+    };
+  }
 
-    if (json.length > passwordProperties.max) {
-      err = new Error (g.f('Password terlalu panjang: %s (maksimal %d karakter)', json, passwordProperties.max));
-      err.code = 'PASSWORD_TOO_LONG';
-    } else if (json.length < passwordProperties.min) {
-      err = new Error(g.f('Password terlalu singkat: %s (minimal %d karakter)', json, passwordProperties.min));
-      err.code = 'PASSWORD_TOO_SHORT';
-    } else if(!(new RegExp(passwordProperties.pattern, 'g').test(json))) {
-      err =  new Error(g.f('password salah: %s (masukkan huruf kapital, huruf kecil, simbol, dan angka)', json));
-      err.code = 'INVALID_PASSWORD';
-    } else {
-      return true;
-    }
-    err.statusCode = 422;
-    throw err;
-  };
-
-  //remote-method membuat saldo_cuti untuk karyawan baru
-  Pengguna.afterRemote('create', function(context, pengguna, next){
-    let today = new Date();
-    app.models.saldo_cuti.create([
-      {id_pengguna : pengguna.id, tanggal_masuk : today,
-      cuti_reguler : 0, cuti_bonus : 0, cuti_bonus_exp : today}
-    ],function(err){
-      if (err) throw (err);
-      console.log("saldo cuti for pengguna " + pengguna.username + " has been created")
-      return next();
+  {//logika saldo_cuti
+    Pengguna.afterRemote('create', function(context, pengguna, next){
+      let today = new Date();
+      app.models.saldo_cuti.create([
+        {id_pengguna : pengguna.id, tanggal_masuk : today,
+        cuti_reguler : 0, cuti_bonus : 0, cuti_bonus_exp : today}
+      ],function(err){
+        if (err){
+          Pengguna.destroyById(pengguna.id);
+          throw (err)
+        };
+        console.log("saldo cuti for pengguna " + pengguna.username + " has been created")
+        return next();
+      })
     })
-  })
+  }
 
-  {//remote-method cuti
-    let reg_before, reg_after, bonus_before, bonus_after, penggunaid, tgl_masuk_after, exp_after, id_after;
+  {//logika cuti
+    let reg_before, reg_after, bonus_before, bonus_after, penggunaid, tgl_masuk_after, exp_after, id_after, id_cuti;
 
     Pengguna.beforeRemote('*.__create__cuti', async (context, err) => {
-      const docs = await app.models.saldo_cuti.find({where:{id_pengguna : context.instance.id}});
-      console.log("pengguna: " + docs[0].id_pengguna + "\ncuti_type: " + context.args.data.cuti_type);
-      let mulai_cuti = new Date(context.args.data.mulai_cuti.substr(0,4), context.args.data.mulai_cuti.substr(5,2) - 1, context.args.data.mulai_cuti.substr(8,2))
-      let selesai_cuti = new Date(context.args.data.selesai_cuti.substr(0,4), context.args.data.selesai_cuti.substr(5,2) - 1, context.args.data.selesai_cuti.substr(8,2))
-      let lama_cuti = selesai_cuti - mulai_cuti;
-      lama_cuti = (lama_cuti + 86400000) / 86400000;
-
       const error = {
         statusCode: "400",
         message: "ERROR : Saldo cuti tidak mencukupi."
       };
+
+      const docs = await app.models.saldo_cuti.find({where:{id_pengguna : context.instance.id}});
+      let mulai_cuti = new Date(context.args.data.mulai_cuti.substr(0,4), context.args.data.mulai_cuti.substr(5,2) - 1, context.args.data.mulai_cuti.substr(8,2))
+      let selesai_cuti = new Date(context.args.data.selesai_cuti.substr(0,4), context.args.data.selesai_cuti.substr(5,2) - 1, context.args.data.selesai_cuti.substr(8,2))
+      let lama_cuti = selesai_cuti - mulai_cuti;
+      lama_cuti = (lama_cuti + 86400000) / 86400000;
       
       reg_before = docs[0].cuti_reguler;reg_after = docs[0].cuti_reguler;
       bonus_before = docs[0].cuti_bonus;bonus_after = docs[0].cuti_bonus;
@@ -111,6 +118,7 @@ module.exports = function(Pengguna) {
       exp_after = docs[0].cuti_bonus_exp;
       id_after = docs[0].id;
       tgl_masuk_after = docs[0].tanggal_masuk;
+      id_cuti = context.args.data.id;
 
       function hitung_cuti(g,h,i){
         let x = g - lama_cuti;
@@ -153,7 +161,6 @@ module.exports = function(Pengguna) {
     
       tgl_pengajuan = new Date;
       context.args.data.tgl_pengajuan = tgl_pengajuan.toLocaleString();
-      console.log(context.args.data.tgl_pengajuan);
       
       console.log(
         "reg_before : " + context.args.data.saldo_reg_awal +
@@ -165,20 +172,19 @@ module.exports = function(Pengguna) {
     })
 
     Pengguna.afterRemote('*.__create__cuti', function(context, unused, next){
-      console.log(id_after);
       app.models.saldo_cuti.replaceById(id_after, {id : id_after, id_pengguna : penggunaid, cuti_reguler : reg_after, cuti_bonus : bonus_after, cuti_bonus_exp : exp_after, tanggal_masuk : tgl_masuk_after}
       ,function(err){
-        if (err) throw (err);
+        if (err){
+          app.models.cuti.destroyById(id_cuti)
+          throw (err);
+        } 
         return next();
       })
     })
   }
-  
 
-  Pengguna.beforeRemote('*.__create__saldo_cuti', function(context, unused, next){
-    {
-      console.log("OK");
-      return next();
-    }
-  })
+  {//logika kehadiran
+
+  }
+
 };
